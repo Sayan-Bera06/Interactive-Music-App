6245bbb29a7c004b5f5f64c1d744eb9c05ca5a12
Sayan Bera <132321762+Sayan-Bera06@users.noreply.github.com>
2025-03-03 16:04:00 +0530

    Add files via upload

---
+++ music.js
@@ -0,0 +1,26 @@
+let trackList = [
+    {
+      name: "chal rastay saji",
+      path: "./music/Chal Rastay Saji Tram Line(PagalWorldl).mp3",
+      img: "./images/image1.jpg",
+      singer: "shreya ghosal",
+    },
+    {
+      name: "Gulabi Aankhen",
+      path: "./music/Gulabi Aankhen - SANAM.mp3",
+      img: "./images/image2.jpg",
+      singer: "sanam puri",
+    },
+    {
+      name: "Jawl Phoring",
+      path: "./music/Jawl Phoring 2.0(PagalWorld.com.se).mp3",
+      img: "./images/image3.jpg",
+      singer: "anupam roy",
+    },
+    {
+      name: "loser",
+      path: "./music/Loser - Dino James 320 Kbps.mp3",
+      img: "./images/image4.jpg",
+      singer: "dino james",
+    },
+  ];
